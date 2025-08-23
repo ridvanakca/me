7b5735f1c81d2fe99923e4dea2a79be623aa1e43
Rıdvan Akca <akca.ridvann@gmail.com>
2025-08-23 15:34:16 +0200

    feat: update hero text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ export default function Page() {
   return (
     <div>
       <h3 className='md:hidden font-semibold text-2xl pb-4'>Home</h3>
-      <p>Hey, I’m Rıdvan 🙌 I’m a software developer, foodie, and analog photography enthusiast who enjoys building, tasting, and capturing based in Munich, Germany.</p>
+      <p>Hey, I’m Rıdvan 🙌 I’m a software developer, foodie, and analog photography enthusiast based in Munich, Germany.</p>
+      <p className='pt-2'>By day, I build web applications as a Fullstack Developer at CHECK24 and pursue my Master’s at TUM. I previously crafted frontends at Automat Berlin. Outside of work, you’ll usually find me exploring Munich’s food scene or capturing moments on my analog camera.</p>
     </div>
   );
 }
